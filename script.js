3ef5a9092eec3b6ddf4ac6c56ca2df2557580136
vamshi <vamshireddy19421@gmail.com>
2025-01-18 05:13:24 +0530

    fixed gsap2

--- script.js
+++ script.js
@@ -1,61 +1,45 @@
-// GSAP animation for navbar items with stagger effect
-gsap.from(".navbar", { 
-    duration: 1, 
-    y: -50, 
-    opacity: 0, 
-    ease: "power2.out",
-    onStart: function() {
-      document.querySelector('.navbar').style.visibility = "visible";
-    }
-  });
-  
-  gsap.from(".hero .left", { 
-    duration: 1.5, 
-    x: -100, 
-    opacity: 0, 
-    ease: "power2.out", 
-    delay: 0.5,
-    onStart: function() {
-      document.querySelector('.hero .left').style.visibility = "visible";
-    }
-  });
-  
-  gsap.from(".hero .right", { 
-    duration: 1.5, 
-    x: 100, 
-    opacity: 0, 
-    ease: "power2.out", 
-    delay: 0.5,
-    onStart: function() {
-      document.querySelector('.hero .right').style.visibility = "visible";
-    }
-  });
-  
-  gsap.from(".buttons button", { 
-    duration: 1, 
-    scale: 0, 
-    opacity: 0, 
-    ease: "elastic.out(1, 0.5)", 
-    delay: 1,
-    onStart: function() {
-      document.querySelector('.buttons button').style.visibility = "visible";
-    }
-  });
-  
-  gsap.from(".navbar ul li", {
-    duration: 2,
-    delay: 1,
-    y: -50, 
-    opacity: 0, 
-    ease: "power2.out",
-    stagger: 0.2,
-    onStart: function() {
-      document.querySelectorAll('.navbar ul li').forEach(item => {
-        item.style.visibility = "visible";
-      });
-    }
-  });
-  
+
+window.onload = function () {
+    gsap.from(".navbar", { 
+        duration: 1, 
+        y: -50, 
+        opacity: 0, 
+        ease: "power2.out"
+    });
+
+    gsap.from(".hero .left", { 
+        duration: 1.5, 
+        x: -100, 
+        opacity: 0, 
+        ease: "power2.out", 
+        delay: 0.5
+    });
+
+    gsap.from(".hero .right", { 
+        duration: 1.5, 
+        x: 100, 
+        opacity: 0, 
+        ease: "power2.out", 
+        delay: 0.5
+    });
+
+    gsap.from(".buttons button", { 
+        duration: 1, 
+        scale: 0, 
+        opacity: 0, 
+        ease: "elastic.out(1, 0.5)", 
+        delay: 1
+    });
+
+    gsap.from(".navbar ul li", {
+        duration: 2,
+        delay: 1,
+        y: -50, 
+        opacity: 0, 
+        ease: "power2.out",
+        stagger: 0.2
+    });
+}
 
 
 const hamburger = document.getElementById('hamburger-icon');
